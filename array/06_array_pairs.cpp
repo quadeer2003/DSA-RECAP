a37e69c30c5cb7c94f267f609abbb730fe391b8e
Mohammad Abdul Quadeer <quadeer2003@gmail.com>
2023-11-02 00:29:29 +0530

    Added dpp

---
+++ array/06_array_pairs.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void print_pairs(int arr[], int n){
+	for(int i = 0; i<n ; i++){
+		int x = arr[i];
+		for(int j=i+1; j<n; j++){
+			int y=arr[j];
+
+			cout<<x<<","<<y<<endl;
+		}
+		cout<<endl;
+	}
+}
+int main(){
+	int arr[100];
+
+	int n;
+	cin>>n;
+
+	for (int i = 0; i < n; i++){
+		cin>>arr[i];
+	}
+
+	print_pairs(arr,n);
+
+}
